66f831cc565cd1896a4e01dd9f3c24a6b9b708bc
unknown <grobamist@gmail.com>
2022-06-01 21:43:40 +0300

    connection string fixed

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 const db = require('mongoose')
-const url =
-    process.env.DB_CONNECT ??
-    'mongodb://localhost:27017/' + process.env.DB_NAME ??
-    'hospital'
+const connect = process.env.DB_CONNECT ?? 'mongodb://localhost:27017/'
+const dbName = process.env.DB_NAME ?? 'hospital'
+const url = connect + dbName
+
+console.log(url)
 db.connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
 module.exports = db
