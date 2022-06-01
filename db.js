36e99f3d48c699d80596c77e124677bfc2e5434f
unknown <grobamist@gmail.com>
2022-06-01 23:31:26 +0300

    docker added

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const db = require('mongoose')
-const connect = process.env.DB_CONNECT ?? 'mongodb://localhost:27017/'
-const dbName = process.env.DB_NAME ?? 'hospital'
+const connect = 'mongodb://mongodb:27017/' //process.env.DB_CONNECT ??
+const dbName = 'hospital' // process.env.DB_NAME ??
 const url = connect + dbName
-
+console.log('Oh, hi Mark!')
 console.log(url)
 db.connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
 module.exports = db
